54ec11810ff17dd05b6eb0f5d1073c0582890fc1
beans_pc <ujt2h8@gmail.com>
2019-08-18 21:10:12 +0800

    [alexander][refactor][countCards]

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,6 +5,8 @@ public class Player {
 
     private List<Card> cards;
     private int level;
+    private List<Map.Entry<String,Integer>> countedCards;
+
 
     Player(List<Card> cards) {
         this.cards = cards;
@@ -20,12 +22,12 @@ public class Player {
         return 2;
     }
 
-    public boolean hasPair() {
-        List<Map.Entry<String,Integer>> countedCards = new ArrayList<>(this.cards.stream()
+
+    public void countCards(){
+        countedCards = new ArrayList<>(this.cards.stream()
                 .map(Card::getNumber)
                 .collect(Collectors.toMap(poker -> poker, poker -> 1, Integer::sum))
                 .entrySet());
-
         countedCards.sort(new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2) {
@@ -33,38 +35,21 @@ public class Player {
             }
 
         });
+    }
 
+    public boolean hasPair() {
         for (Map.Entry<String, Integer> entry : countedCards) {
             if( entry.getValue() == 2)
                 return true;
         }
-
         return false;
-
-
     }
 
     public boolean hasThree() {
-        List<Map.Entry<String,Integer>> countedCards = new ArrayList<>(this.cards.stream()
-                .map(Card::getNumber)
-                .collect(Collectors.toMap(poker -> poker, poker -> 1, Integer::sum))
-                .entrySet());
-
-        countedCards.sort(new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue()) * -1;
-            }
-
-        });
-
         for (Map.Entry<String, Integer> entry : countedCards) {
             if( entry.getValue() == 3)
                 return true;
         }
-
         return false;
-
-
     }
 }
