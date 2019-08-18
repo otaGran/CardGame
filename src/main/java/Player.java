3fdaa6a23d89a6cdde5b57a36eceffce405e43c6
beans_pc <ujt2h8@gmail.com>
2019-08-18 18:37:32 +0800

    [alexander][impl][should_return_all_cards_player_had_when_call_getCards]

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,13 @@
+import java.util.List;
+
 public class Player {
 
+    private List<Card> cards;
+    Player(List<Card> cards){
+        this.cards = cards;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
