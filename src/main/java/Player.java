f8c3414a34110d159ea03162834353030dc362fb
beans_pc <ujt2h8@gmail.com>
2019-08-18 20:39:16 +0800

    [alexander][impl][should_return_2_when_players_has_only_one_pair]

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,11 +3,17 @@ import java.util.List;
 public class Player {
 
     private List<Card> cards;
+    private int level;
     Player(List<Card> cards){
         this.cards = cards;
-    }
 
+
+    }
     public List<Card> getCards() {
         return cards;
+    }
+
+    public int getLevel(){
+        return 2;
     }
 }
