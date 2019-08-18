524f21317938e7b9a39129162f9347a83faa2079
beans_pc <ujt2h8@gmail.com>
2019-08-18 21:06:24 +0800

    [alexander][impl][should_return_true_when_players_has_three_of_a_kind]

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -43,4 +43,28 @@ public class Player {
 
 
     }
+
+    public boolean hasThree() {
+        List<Map.Entry<String,Integer>> countedCards = new ArrayList<>(this.cards.stream()
+                .map(Card::getNumber)
+                .collect(Collectors.toMap(poker -> poker, poker -> 1, Integer::sum))
+                .entrySet());
+
+        countedCards.sort(new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue()) * -1;
+            }
+
+        });
+
+        for (Map.Entry<String, Integer> entry : countedCards) {
+            if( entry.getValue() == 3)
+                return true;
+        }
+
+        return false;
+
+
+    }
 }
