eb04013011f138beae54170e8c0746bc9299d7b8
OtaGran <ujt2h8@gmail.com>
2019-08-19 12:35:57 +0800

    [alexander][impl][should_print_Players_1_win_when_reveive_1H2H3H4H5H_1D3D5D6S7D]

--- src/main/java/Judgement.java
+++ src/main/java/Judgement.java
@@ -1,2 +1,20 @@
 public class Judgement {
+    Judgement(){
+
+    }
+
+    public String judge(Player player1, Player player2){
+        int players1Level = player1.getLevel();
+        int players2Level = player2.getLevel();
+
+        if(players1Level == players2Level){
+
+        }
+        else{
+            if(players1Level > players2Level)
+                return "Player 1 win";
+            else
+                return "Player 2 win";
+        }
+    }
 }
